82422ddc88a02f38a9d1e6d94a5d872a4c8af70a
Robert Grubba <rgrubba@gmail.com>
2022-10-11 13:30:47 +0200

    base admin functionality + templates

--- scratchpad/scratchpad/urls.py
+++ scratchpad/scratchpad/urls.py
@@ -13,9 +13,26 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += [
+    path('',views.index,name='index'),
+    path('about',views.about,name='about'),
+    path('services',views.services,name='services'),
+    path('portfolio',views.portfolio,name='portfolio'),
+    path('team',views.team,name='team'),
+    path('blog',views.blog,name='blog'),
+    path('pricing',views.pricing,name='pricing'),
+    path('contact',views.contact,name='contact'),
+]
+
+urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+
---
+++ scratchpad/scratchpad/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def portfolio(request):
+    return render(request, 'portfolio.html')
+
+def team(request):
+    return render(request, 'team.html')
+
+def pricing(request):
+    return render(request, 'pricing.html')
+
+def blog(request):
+    return render(request, 'blog.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
