fbc16762d7ab9d980893fc3b8ee700c0284ff89e
Robert Grubba <rgrubba@gmail.com>
2022-10-20 15:06:24 +0200

    improvement of templates

---
+++ scratchpad/scratchpad/storage_backends.py
@@ -0,0 +1,11 @@
+from django.conf import settings
+from storages.backends.s3boto3 import S3Boto3Storage
+
+class MediaStorage(S3Boto3Storage):
+    location = 'media'
+    file_overwrite = False
+
+class PublicMediaStorage(S3Boto3Storage):
+    location = settings.AWS_PUBLIC_MEDIA_LOCATION
+    file_overwrite = False
+    default_acl = 'public'
